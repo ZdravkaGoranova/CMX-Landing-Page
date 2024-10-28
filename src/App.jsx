9d2e8ddf87dc8a770facd1094f424de9d4db0cd0
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-10-28 21:16:07 +0200

    update Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './Components/Footer.jsx';
 import Header from './Components/Header.jsx';
 import Main from './Components/Main.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
