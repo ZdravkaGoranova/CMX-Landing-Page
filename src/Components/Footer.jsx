4a7b5120fd4d8771067d5a4c6bd49329b5b71986
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-10-28 23:27:54 +0200

    adding dark/white theme into Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,87 +1,182 @@
-// import Nav from 'react-bootstrap/Nav';
-
 import './Footer.css';
-<link
-  href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-  rel="stylesheet"
-/>;
-const Footer = () => {
+import PropTypes from 'prop-types';
+
+const Footer = ({ theme }) => {
   return (
     <>
-      {/* <link
+      <link
         href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
         rel="stylesheet"
       />
 
-   
-       <footer className="bg-dark footer">
-    <div className="footer-top py-8">
+      <footer
+        className={`footer_area navbar fixed-bottom bg-body-tertiary`}
+        data-bs-theme={theme === 'dark' ? 'dark' : undefined}
+      >
         <div className="container">
-            <div className="row gy-5">
-                <div className="col-lg-8 pe-xxl-10">
-                    <div className="row gy-5">
-                        <div className="col-6 col-lg-4">
-                            <h5 className="text-white footer-title-01">A propos</h5>
-                            <ul className="list-unstyled footer-link-01 m-0">
-                                <li><a className="text-white text-opacity-75" href="#">About Us</a></li>
-                                <li><a className="text-white text-opacity-75" href="#">Blog</a></li>
-                                <li><a className="text-white text-opacity-75" href="#">Our ingredients</a></li>
-                                <li><a className="text-white text-opacity-75" href="#">Our resellers</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-6 col-lg-4">
-                            <h5 className="text-white footer-title-01">About</h5>
-                            <ul className="list-unstyled footer-link-01 m-0">
-                                <li><a className="text-white text-opacity-75" href="#">Customer Service</a></li>
-                                <li><a className="text-white text-opacity-75" href="#">Privacy Policy</a></li>
-                                <li><a className="text-white text-opacity-75" href="#">Cookie Policy</a></li>
-                                <li><a className="text-white text-opacity-75" href="#">Terms &amp; Conditions</a></li>
-                                <li><a className="text-white text-opacity-75" href="#">Terms of Use</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-6 col-lg-4">
-                            <h5 className="text-white footer-title-01">Need Help?</h5>
-                            <ul className="list-unstyled footer-link-01 m-0">
-                                <li><a className="text-white text-opacity-75" href="#">Contact Us</a></li>
-                                <li><a className="text-white text-opacity-75" href="#">FAQs</a></li>
-                                <li><a className="text-white text-opacity-75" href="#">Offers &amp; Kits</a></li>
-                                <li><a className="text-white text-opacity-75" href="#">Get the app</a></li>
-                                <li><a className="text-white text-opacity-75" href="#">Store locator</a></li>
-                            </ul>
-                        </div>
-                    </div>
+          <div className="row">
+            <div className="col-12 col-sm-6 col-lg-4">
+              <div className="single-footer-widget section_padding_0_130">
+                <div className="footer-logo mb-3"></div>
+
+                <div className="footer_social_area">
+                  <a
+                    href="https://www.facebook.com/CMX.BG"
+                    data-toggle="tooltip"
+                    data-placement="top"
+                    title=""
+                    data-original-title="Facebook"
+                  >
+                    <i className="fa fa-facebook"></i>
+                  </a>
+                  <a
+                    href="https://www.youtube.com/channel/UCqElYlVbs68ohjPetcSMjcA"
+                    data-toggle="tooltip"
+                    data-placement="top"
+                    title=""
+                    data-original-title="Pinterest"
+                  >
+                    <i className="fa fa-youtube"></i>
+                  </a>
                 </div>
-                <div className="col-lg-4">
-                    <h5 className="text-white footer-title-01 fs-5">Subscribe to our newsletter and get 15% off your next order.</h5>
-                    <div>
-                        <form className="d-flex flex-row mb-2 p-1 bg-white input-group"><input type="email" className="form-control rounded border-0" placeholder="Your Email"/> <button className="btn btn-secondary flex-shrink-0" type="submit">Subscribe</button></form>
-                        <p className="fs-sm text-white text-opacity-75">I agree to receive Absolution newsletters</p>
-                    </div>
-                </div>
+              </div>
             </div>
-        </div>
-    </div>
-    <div className="footer-bottom small py-3 border-top border-white border-opacity-10">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6 text-center text-md-start py-1">
-                    <p className="m-0 text-white text-opacity-75">© 2022 copyright by <a className="text-reset" href="#">bootdey.com</a></p>
+
+            <div className="col-12 col-sm-6 col-lg">
+              <div className="single-footer-widget section_padding_0_130">
+                <h5
+                  className={`widget-title ${
+                    theme === 'dark' ? 'text-white' : 'text-black'
+                  }`}
+                >
+                  About
+                </h5>
+
+                <div className="footer_menu">
+                  <ul>
+                    <li>
+                      <a href="https://www.cmx.bg/page/1/za-nas">За нас</a>
+                    </li>
+                    <li>
+                      <a href="https://www.cmx.bg/main/index/contacts">
+                        Контакти
+                      </a>
+                    </li>
+                  </ul>
                 </div>
-                <div className="col-md-6 text-center text-md-end py-1">
+              </div>
+            </div>
+
+            <div className="col-12 col-sm-6 col-lg">
+              <div className="single-footer-widget section_padding_0_130">
+                <h5
+                  className={`widget-title ${
+                    theme === 'dark' ? 'text-white' : 'text-black'
+                  }`}
+                >
+                  Support
+                </h5>
+
+                <div className="footer_menu">
+                  <ul>
+                    <li>
+                      <a href="https://www.cmx.bg/page/2/vrashtane-na-produkt">
+                        Връщане на продукт
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.cmx.bg/page/3/usloviya-za-polzvane">
+                        Условия за ползване
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-sm-6 col-lg">
+              <div className="single-footer-widget section_padding_0_130">
+                <h5
+                  className={`widget-title ${
+                    theme === 'dark' ? 'text-white' : 'text-black'
+                  }`}
+                >
+                  Delivery
+                </h5>
+
+                <div className="footer_menu">
+                  <ul>
+                    <li>
+                      <a
+                        href="https://www.cmx.bg/page/4/kak-da-poracham-onlayn"
+                     
+                      >
+                        Как да поръчам онлайн?
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.cmx.bg/page/5/dostavka"
+                       
+                      >
+                        Доставка
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className={`footer-bottom small py-3 border-top ${
+                theme === 'dark'
+                  ? 'border-white border-opacity-10'
+                  : 'border-black border-opacity-10'
+              }`}
+            >
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-6 text-center text-md-start py-1">
+                    <p
+                      className={`m-0 ${
+                        theme === 'dark'
+                          ? 'text-white text-opacity-75'
+                          : 'text-black text-opacity-75'
+                      }`}
+                      data-bs-theme={theme === 'dark' ? 'dark' : undefined}
+                    >
+                      © 2024 Copyright cmx.bg. Всички права запазени.
+                    </p>
+                  </div>
+                  <div className="col-md-6 text-center text-md-end py-1">
                     <ul className="nav justify-content-center justify-content-md-end list-unstyled footer-link-01 m-0">
-                        <li className="p-0 mx-3 ms-md-0 me-md-3"><a href="#" className="text-white text-opacity-75">Privace &amp; Policy</a></li>
-                        <li className="p-0 mx-3 ms-md-0 me-md-3"><a href="#" className="text-white text-opacity-75">Faq's</a></li>
-                        <li className="p-0 mx-3 ms-md-0 me-md-3"><a href="#" className="text-white text-opacity-75">Get a Quote</a></li>
+                      <li className="p-0 mx-3 ms-md-0 me-md-3">
+                        <a
+                          href="#"
+                          className={`m-0 ${
+                            theme === 'dark'
+                              ? 'text-white text-opacity-75'
+                              : 'text-black text-opacity-75'
+                          }`}
+                          data-bs-theme={theme === 'dark' ? 'dark' : undefined}
+                        >
+                          Privace &amp; Policy
+                        </a>
+                      </li>
                     </ul>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</footer>  */}
+      </footer>
     </>
   );
 };
 
 export default Footer;
 
-
+Footer.propTypes = {
+  theme: PropTypes.string.isRequired,
+};
